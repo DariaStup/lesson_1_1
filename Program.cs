e2587f98ea915777c63b809d05d85901c3f16b59
stupnikova <stupnikova_dv@ascon.ru>
2024-09-23 21:26:05 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите Имя");
+string? firstname = Console.ReadLine();
+Console.WriteLine("Введите Фамилию");
+string? lastname = Console.ReadLine();
+Console.WriteLine("Введите Отчество");
+string? middlename = Console.ReadLine(); 
+Console.WriteLine("ФИО: " + lastname + " " + firstname + " " + middlename);
